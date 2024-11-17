0912bcc183c014e8eab2da05dc9fc4bc1694ca68
Joseph Meeks <jmeeks33@gatech.edu>
2024-11-17 16:57:24 -0500

    Create Data Translator Updater.py

---
+++ 7 Data Translation/Data Translator Updater.py
@@ -0,0 +1,107 @@
+
+
+# This is the code required to overwrite and update the imports, files, etc.
+
+
+import os
+
+#Imports necessary modules to run this code
+os.system("py -m pip install --no-input requests")
+os.system("py -m pip install --no-input importlib.util")
+
+import requests #for accessing API
+import importlib.util #for importing
+
+#List of libraries/modules to be installed
+libraryNameList = []
+
+#Appends libraries/modules to list
+def imports(savePath):
+    file = open(savePath, encoding="ISO-8859-1") #only encoding that seems to work
+    for line in file:
+        line = line.strip()
+        if not line:
+            continue
+        if line.startswith("import "):
+            lineList = line.split()
+            libraryNameList.append(lineList[1])
+        elif line.startswith("from"):
+            lineList = line.split()
+            libraryNameList.append(lineList[1])
+    
+    return libraryNameList
+
+#Accesses GitAPI to create list of folders in Github repo to be downloaded
+def listFilesInFolder(gitUrl, folderPath):
+    apiUrl = f"{gitUrl.replace('github.com', 'api.github.com/repos')}/contents/{folderPath}"
+    response = requests.get(apiUrl)
+    items = response.json()
+    if isinstance(items, list):
+        return items
+    else:
+        print(f"Didn't work loser! Tried {items}")
+        return []
+
+#Uses OS to download every file
+def downloadFile(gitUrl, filePath, savePath):
+    if filePath == "7 Data Translation/Data Translator Installer Tool.py":
+        print(f"Skipping {filePath}...")
+        return
+    url = f"{gitUrl}/raw/main/{filePath}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        os.makedirs(os.path.dirname(savePath), exist_ok=True) #creates folder to install to
+        with open(savePath, 'wb') as f:
+            f.write(response.content) #Downloads file
+        print(f"Downloaded {filePath} to {savePath}")
+        imports(savePath)
+    else:
+        print(f"Failed to download {filePath}")
+
+#If the value in the API is a folder (dir), goes deeper into API until folder is reached,
+#then adds folders to be downloaded
+def downloadFolder(gitUrl, folderPath, saveFolder):
+    items = listFilesInFolder(gitUrl, folderPath)
+    if items:
+        for item in items:
+            itemPath = item['path'] #folder path
+            itemType = item['type'] #checks whether item is a FILE or FOLDER (directory
+            localSavePath = os.path.join(saveFolder, itemPath) #creates save path
+            if itemType == 'file': #if item is a FILE
+                os.makedirs(os.path.dirname(localSavePath), exist_ok=True)
+                downloadFile(gitUrl, itemPath, localSavePath)
+            elif itemType == 'dir': #if item is a FOLDER
+                downloadFolder(gitUrl, itemPath, saveFolder)
+
+#User inputs, github URL
+gitUrl = "https://github.com/Georgia-Tech-Off-Road/GTOR-DAQ"
+saveFolder = input(r"Enter the local folder path where the file will be saved (e.g., C:\Users\<name>\Documents): ")
+folderPath = input(r"Enter the folder path within the GitHub repository that you want to download (e.g., 7%20Data%20Translation). If you want to download the Data Translation folder, type data: ")
+
+#Defaults to Data Translation if user inputs 'data'
+if folderPath == "data":
+    folderPath = "7%20Data%20Translation"
+
+#Removes quotes if user uses them in folder paths
+folderPath = folderPath.replace("\"","")
+saveFolder = saveFolder.replace("\"","")
+
+#Calls downloadFolder function to download files to saveFolder
+downloadFolder(gitUrl, folderPath, saveFolder)
+
+#Loops through items in libraryNameList to import necessary modules/libraries
+print(libraryNameList) #unnecessary but helpful for debugging
+for library in libraryNameList:
+    print(f"Processing: '{library}'")
+    if importlib.util.find_spec(library):
+        print(f"{library} is already installed. Skipping...")
+        continue
+    result = os.system(f"py -m pip install --no-input {library}")
+    if result == 0:
+        print(f"Successfully installed {library}.")
+    else:
+        print(f"Failed to install {library}. Error code: {result}")
+    
+
+print("Done!")
+
